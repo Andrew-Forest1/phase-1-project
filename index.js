68ed52f4a19ba6be3011b80582f9daa42b3a51a4
Andrew <aforest41@yahoo.com>
2022-12-05 16:26:59 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById("container")
+    const r2 = document.getElementById("R2-D2")
+
+    fetch('https://swapi.dev/api/species')
+    .then(res => res.json())
+    .then(data => data.results.forEach(renderSpecies))
+    
+    r2.addEventListener('mouseenter', (e) => {
+        e.target.classList.remove('pictures')
+        e.target.classList.add('hover')
+    })
+
+    r2.addEventListener('mouseleave', (e) => {
+        e.target.classList.remove('hover')
+        e.target.classList.add('pictures')
+    })
+    
+    const renderSpecies = (specieObj) => {
+        const newP = document.createElement('p')
+        console.log(specieObj)
+        newP.innerText = specieObj.name
+        container.appendChild(newP);
+    }
+})
+
