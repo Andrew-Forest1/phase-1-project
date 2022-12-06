57561342459322eb412ed97646bd9f859b616129
Andrew <aforest41@yahoo.com>
2022-12-06 10:55:26 -0800

    Added Form and Species Pictures

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById("container")
-    const r2 = document.getElementById("R2-D2")
+    //const r2 = document.getElementById("R2-D2")
 
     fetch('http://localhost:3000/species')
     .then(res => res.json())
     .then(data => data.forEach(renderSpecies))
     
-    r2.addEventListener('mouseenter', (e) => {
+/*     r2.addEventListener('mouseenter', (e) => {
         e.target.classList.remove('pictures')
         e.target.classList.add('hover')
     })
@@ -14,14 +14,27 @@ document.addEventListener('DOMContentLoaded', () => {
     r2.addEventListener('mouseleave', (e) => {
         e.target.classList.remove('hover')
         e.target.classList.add('pictures')
-    })
+    }) */
     
     const renderSpecies = (specieObj) => {
-        const newP = document.createElement('p')
-        console.log(specieObj)
-        newP.innerText = specieObj.name
-        container.appendChild(newP);
+        const newImg = document.createElement('img')
+        //console.log(specieObj)
+        newImg.innerText = specieObj.name
+        newImg.src = specieObj.image
+        newImg.alt = specieObj.name
+        newImg.classList.add('pictures')
+
+        newImg.addEventListener('mouseleave', (e) => {
+            e.target.classList.remove('hover')
+            e.target.classList.add('pictures')
+        })
+
+        newImg.addEventListener('mouseenter', (e) => {
+            e.target.classList.remove('pictures')
+            e.target.classList.add('hover')
+        })
+        container.appendChild(newImg);
     }
 })
 
-console.log('yo')
+//console.log('yo')
